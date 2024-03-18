7e7b4417891fb67cd9177c13f0ce78c7b265620e
moonspark78 <115048076+moonspark78@users.noreply.github.com>
2024-03-18 09:53:09 +0100

    :art: test 2  :art:

--- test1.js
+++ test1.js
@@ -18,10 +18,36 @@ console.log(dec);
 
 // -------------------------------------------------------------
 const people= [{name: 'John', age: 19},
-               {name: 'alban', age: 7},
-               {name: 'tian', age: 54},
-               {name: 'marc', age: 24},
-               {name: 'zoro', age: 11},]
+{name: 'alban', age: 7},
+{name: 'tian', age: 54},
+{name: 'marc', age: 24},
+{name: 'zoro', age: 11},]
 
 people.sort((a, b) => a.age - b.age); 
 console.log(people);
+
+// -------------------------------------------------------------
+const tian = "elephant"
+mots= tian.split("")
+console.log(mots);
+
+// -------------------------------------------------------------
+let fix = [12,22,46,78,3,56,1,0]
+lean=Math.max(...fix)
+console.log(lean);
+
+//ou
+
+console.log(Math.max(1,45,64,888))
+// -------------------------------------------------------------
+
+const isBool = ((a,b) =>{
+    if(a ===1 || b ===1 || a + b ===1){
+        return true
+    }else {
+        return false
+    }
+
+})
+ze =isBool(3,8);
+console.log(ze);
