5baa0985ae0afb4b6c3eb10632a656ff3b7228ba
moonspark78 <115048076+moonspark78@users.noreply.github.com>
2024-03-20 04:07:55 +0100

    :art: day 2   :art:

---
+++ day2/test2.js
@@ -0,0 +1,43 @@
+//let paragraphe = "Le développement web est une compétence essentielle pour les programmeurs modernes.";
+//let tes = paragraphe.split(" ");
+//let chaine = tes.reverse();
+//console.log(chaine);
+//let finale =chaine.join(" ");
+//console.log(finale);
+
+/* let paragraphe = "Le développement web est une compétence essentielle pour les programmeurs modernes.";
+let sep = paragraphe.split(" ");
+console.log(sep);
+let ko = sep.join("-");
+console.log(ko);
+*/
+
+let paragraphe ="Le développement web est une compétence essentielle pour les programmeurs modernes.";
+let ser = paragraphe.split(" ");
+console.log(ser);
+
+// Enlever le premier mot 
+        //ser.shift();
+        //console.log(ser);
+
+
+// Enlever le dernier mot
+        //ser.pop();
+        //console.log(ser);
+
+// Ajouter un mot au début
+        //ser.unshift("messi");
+        //console.log(ser);
+
+
+
+
+// Ajoute un mot a la fin 
+      //ser.push("lola")
+      //console.log(ser);
+
+//ser.splice(1, 9, "messi","leo")
+//console.log(ser);
+
+let to =ser.slice(1,4)
+console.log(to);
