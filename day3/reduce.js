a98b38662e1d506252e8d1c3f10703139c0c1c93
moonspark78 <115048076+moonspark78@users.noreply.github.com>
2024-03-20 17:33:33 +0100

    :art: day 3   :art:

--- day3/reduce.js
+++ day3/reduce.js
@@ -26,13 +26,21 @@ console.log("Le plus grand nombres est :" ,max);
 //------
 const words = ["chat", "éléphant", "arbre", "voiture", "banane", "le"];
 // Le mots le plus long: 
-
+const plusLongMots = words.reduce((acc,cur) => {
+    return acc.length > cur.length ? acc : cur
+},words[0])
+console.log("Le plus long mots est :" ,plusLongMots);
 
 
 //Le mot le plus court :
+const plusPetitMot = words.reduce((acc,cur) =>{
+    return acc.length > cur.length ? cur : acc
+},words[0])
+console.log("Le plus petit mots est : ", plusPetitMot);
 
 
-// le plus jeune de la liste
+
+// obj
 const people = [
     { name: 'John', age: 25 },
     { name: 'John', age: 18 },
@@ -40,9 +48,24 @@ const people = [
     { name: 'Jane', age: 11 },
     { name: 'Alice', age: 20 }
 ];
+// le plus jeune de la liste
+const jeune = people.reduce((acc,cur) => {
+    return acc.age > cur.age ?cur : acc 
+},people[0])
+console.log("Le plus jeune est : ",jeune);
 
+// le plus vieux de la liste
+const vieux = people.reduce((acc, cur) =>{
+    return acc.age > cur.age ? acc : cur
+},people[0])
+console.log("Le plus vieux est : ",vieux);
 
 
 
 //---- transformer un tableau en une chaine
 const stringsArray = ["apple", "banana", "orange", "grape", "pineapple"];
+const chain = stringsArray.reduce((acc,cur) =>{
+    return acc + " " + cur
+})
+console.log("phrase : ",chain);
+console.log(typeof(chain));
